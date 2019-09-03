136c391c04fee5c9b11c0b5b2d1a5ac64a5d4efa
Nishant Rodrigues <nishantjr@gmail.com>
2019-09-04 01:17:31 +0530

    App.java: sayHello

--- hello-world/src/main/java/com/example/tutorial/App.java
+++ hello-world/src/main/java/com/example/tutorial/App.java
@@ -2,12 +2,15 @@ package com.example.tutorial;
 
 /**
  * Hello world!
- *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println(sayHello("cs427"));
+    }
+
+    public static String sayHello(String who) {
+        return "Hello, " + who + "!";
     }
 }
